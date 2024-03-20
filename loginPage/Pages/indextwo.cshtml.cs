a75161f7c78c277ce16a3e93a04bc25793dc7d08
Matsonj30 <matsonj2013@gmail.com>
2024-03-20 17:16:35 -0600

    initialized after much suffering and learned how to open a file

---
+++ loginPage/Pages/indextwo.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyApp.Namespace
+{
+    public class indextwoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
